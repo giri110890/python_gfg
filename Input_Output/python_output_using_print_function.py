64d08745577eb830c2cce76c8188040a88b1eb18
Giridhar Punnamaraju <pgiridhar@75f.io>
2020-03-30 14:32:00 +0530

    Renaming the file and finishing the tutorial

--- Input_Output/python_output_using_print_function.py
+++ Input_Output/python_output_using_print_function.py
@@ -12,4 +12,13 @@ print("x =", x) # output x = 5
 # output can only concatenate string not int
 # notice the space difference before and after
 print("x =" + str(x)) # output x =5 
-                
+
+# code for disabling the softspace feature
+print('G', 'F', 'G', sep = '')
+
+# code without softspace feature
+print('G', 'F', 'G')
+
+# using end argument
+print("Python", end= '@')
+print("GeeksforGeeks")                
